7ed531031ed0b11a02077edad9aa894fe90dc3b1
myc <myc@qq.com>
2021-02-23 16:23:05 +0800

    新增test.java

--- git/src/main/java/com/sc/git/Hello.java
+++ git/src/main/java/com/sc/git/Hello.java
@@ -2,7 +2,7 @@ package com.sc.git;
 
 public class Hello {
     private int i=10;
-
+    private int cc=50;
     public static void main(String[] args) {
         System.out.println("你好！李家键！");
     }
